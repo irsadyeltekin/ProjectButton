fd5c82f79a38571020765d68c70d6371cce077d1
Seda Keskin <seda.keskin@matriksdata.com>
2018-08-02 14:10:42 +0300

    FirstProjectCommit

---
+++ app/src/main/java/com/example/sedakeskin/mybut/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sedakeskin.mybut;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mbutton = (Button) findViewById(R.id.clickOn);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }
+
+}
